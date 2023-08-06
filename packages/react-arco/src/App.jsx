df22f5e636197469133e59ba03cab15c36f72991
许鹏 <3053949745@qq.com>
2023-08-06 18:57:57 +0800

    react-arco

---
+++ packages/react-arco/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from '@arco-design/web-react';
+// import '@arco-design/web-react/dist/css/arco.css';
+
+function App() {
+
+  return (
+    <>
+      <Button type="primary">hello arco</Button>
+    </>
+  );
+}
+
+export default App;
