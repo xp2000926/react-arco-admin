68248ecbb40549183562a077bb1d26f96e78a010
xp2000926 <3053949745@qq.com>
2023-09-10 10:36:47 +0800

    数据请求最佳实践

--- packages/react-arco/src/App.jsx
+++ packages/react-arco/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
-import { Table, Layout, Space, Button } from "@arco-design/web-react";
-import { useEffect, useState } from "react";
+import { Table, Layout, Space, Button } from '@arco-design/web-react';
+import { useEffect, useState } from 'react';
+import { useRequest, usePagination } from 'ahooks';
 const { Header, Sider, Content, Footer } = Layout;
 
 // 基础属性：
@@ -8,90 +9,109 @@ const { Header, Sider, Content, Footer } = Layout;
 // data 数据表格的数据
 const data = [
   {
-    key: "1",
-    name: "Jane Doe",
+    key: '1',
+    name: 'Jane Doe',
     salary: 23000,
-    address: "32 Park Road, London",
-    email: "jane.doe@example.com",
+    address: '32 Park Road, London',
+    email: 'jane.doe@example.com',
   },
   {
-    key: "2",
-    name: "Alisa Ross",
+    key: '2',
+    name: 'Alisa Ross',
     salary: 25000,
-    address: "35 Park Road, London",
-    email: "alisa.ross@example.com",
+    address: '35 Park Road, London',
+    email: 'alisa.ross@example.com',
   },
   {
-    key: "3",
-    name: "Alisa Rosssssss",
+    key: '3',
+    name: 'Alisa Rosssssss',
     salary: 25000,
-    address: "35 Park Road, London",
-    email: "alisa.ross@example.com",
+    address: '35 Park Road, London',
+    email: 'alisa.ross@example.com',
   },
 ];
 
-const getTableData = ({ current, pageSize }) => ({
-  data: data.slice((current - 1) * pageSize, current * pageSize),
-  total: data.length,
-});
+//修改为异步请求
+const getTableData = ({ current, pageSize }) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: data.slice((current - 1) * pageSize, current * pageSize),
+        total: data.length,
+      });
+    }, 1000);
+  });
+};
+
+//userequest
+//usePagination
 
 function App() {
-
-  // 表格数据
-  const [data, setData] = useState([]);
   // 分页信息
-  const [pagination, setPagination] = useState({
-    current: 1,
-    pageSize: 2,
-    total: 0,
+  // const [pagination, setPagination] = useState({
+  //   current: 1,
+  //   pageSize: 2,
+  //   total: 0,
+  // });
+  // // 表格数据
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   const {data, total} = getTableData(pagination);
+  //   setData(data)
+  //   setPagination({...pagination, total})
+  // }, [pagination.current, pagination.pageSize]);
+  // const { data, loading } = useRequest(() => getTableData(pagination), {
+  //   onSuccess: ({ total }) => setPagination({ ...pagination, total }),
+  //   refreshDeps: [pagination.current, pagination.pageSize],
+  // });
+  
+  // 声明式
+  // axios 编程式
+  const { data, pagination, loading } = usePagination(getTableData, {
+    defaultCurrent: 1,
+    defaultPageSize: 2,
   });
-  
-  useEffect(() => {
-    const {data, total} = getTableData(pagination);
-    setData(data)
-    setPagination({...pagination, total})
-  }, [pagination.current, pagination.pageSize]);
 
-  const onChange = (pagination) => {
-    setPagination(pagination)
-  }
-  
+  // const onChange = (pagination) => {
+  //   setPagination(pagination);
+  // };
+
   const tableCallback = (record, operation) => {
-    console.log("操作", operation);
-    console.log("记录", record);
+    console.log('操作', operation);
+    console.log('记录', record);
   };
 
   const columns = [
     {
-      title: "Name",
-      dataIndex: "name",
+      title: 'Name',
+      dataIndex: 'name',
     },
     {
-      title: "Salary",
-      dataIndex: "salary",
+      title: 'Salary',
+      dataIndex: 'salary',
     },
     {
-      title: "Address",
-      dataIndex: "address",
+      title: 'Address',
+      dataIndex: 'address',
     },
     {
-      title: "Email",
-      dataIndex: "email",
+      title: 'Email',
+      dataIndex: 'email',
     },
     {
-      title: "Operation",
-      dataIndex: "operations",
+      title: 'Operation',
+      dataIndex: 'operations',
       render: (_, record) => (
         <>
-          <Button onClick={() => tableCallback(record, "edit")}>编辑</Button>
-          <Button onClick={() => tableCallback(record, "delete")}>删除</Button>
+          <Button onClick={() => tableCallback(record, 'edit')}>编辑</Button>
+          <Button onClick={() => tableCallback(record, 'delete')}>删除</Button>
         </>
       ),
     },
   ];
 
   return (
-    <Layout style={{ height: "400px" }}>
+    <Layout style={{ height: '400px' }}>
       <Header>
         <Space>
           <Button type="primary">Item1</Button>
@@ -103,7 +123,12 @@ function App() {
       <Layout>
         <Sider>Sider</Sider>
         <Content>
-          <Table data={data} columns={columns} pagination={pagination} onChange={onChange}></Table>
+          <Table
+            loading={loading}
+            data={data?.data}
+            columns={columns}
+            pagination={pagination}
+          />
         </Content>
       </Layout>
       <Footer>Footer</Footer>
