1f809dd11115ab997dadf1f7b794fb52b23f5726
xp2000926 <3053949745@qq.com>
2023-09-30 12:08:22 +0800

    next

---
+++ packages/cms/src/app/(auth)/dashboard/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+import styles from './styles.module.scss';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+export default function Dashboard() {
+  const router = useRouter();
+  const jumtTo = () => router.push('/list/3');
+
+  return (
+    <div>
+      <h2 className={styles.dashboard}>dashboard page</h2>
+      <ul>
+        <li>
+          <Link href="/list/1" className="underline">
+            列表项1
+          </Link>
+        </li>
+        <li>
+          <Link href="/list/2" className="underline">
+            列表项2
+          </Link>
+        </li>
+        {/* <botton onClick={jumtTo} className="underline">
+          列表项3
+        </botton> */}
+        <botton onClick={jumtTo} className="btn btn-primary">
+          列表项3
+        </botton>
+      </ul>
+    </div>
+  );
+}
---
+++ packages/cms/src/app/(auth)/list/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function Page({ params }) {
+  return (
+    <div>
+      <h3>list item {params.id}</h3>
+    </div>
+  );
+}
